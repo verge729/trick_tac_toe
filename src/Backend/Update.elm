18096bcbe85505e07018db494c63c6521c5e0114
Verge <m.verge97@gmail.com>
2023-09-13 14:27:04 -0700

    added updates to user connectivity on client disconnect.

--- src/Backend/Update.elm
+++ src/Backend/Update.elm
@@ -1,7 +1,13 @@
 module Backend.Update exposing (update)
 
 import Types 
-
+import Types.Storage.User as User
+import Types.Storage.Game as Game
+import Lamdera exposing (ClientId)
+import Types.Storage.Connectivity as Connectivity
+import Types.Storage.Storage as Storage
+import Dict
+import Types.Storage.Response as Response
 
 update : Types.BackendMsg -> Types.BackendModel -> ( Types.BackendModel, Cmd Types.BackendMsg )
 update msg model =
@@ -16,4 +22,110 @@ update msg model =
             ( model, Cmd.none )
 
         Types.ClientDisconnected session_id client_id ->
-            ( model, Cmd.none )
+            let
+                m_user =
+                    Dict.values model.user_store
+                        |> List.filter (\user -> user.state == Connectivity.Connected client_id)
+                        |> List.head 
+            in
+            case m_user of
+                Just user ->
+                    let
+                        updated_user =
+                            { user | state = Connectivity.Disconnected }
+                        updated_user_store =
+                            Dict.insert user.handle updated_user model.user_store
+                        updated_game_store =
+                            updateConnectivityOnGames updated_user model.game_store
+
+                        clients_to_update =
+                            clientsToUpdateGames updated_user (Dict.values updated_game_store)
+
+                        cmds =
+                            List.map
+                                (\( client, player ) ->
+                                    Lamdera.sendToFrontend client (Types.RequestGamesResponse (Response.SuccessRequestGames <| Game.getGames model.game_store player))
+                                )
+                                clients_to_update
+                    in
+                    ( { model
+                        | user_store = updated_user_store
+                        , game_store = updated_game_store
+                      
+                    }
+                    , Cmd.batch cmds 
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none )
+clientsToUpdateGames : User.User -> List Game.Game -> List ( ClientId, User.User )
+clientsToUpdateGames user list_games =
+    List.foldl
+        (\game clients ->
+            case game.player_two of
+                Just player_two ->
+                    if player_two.handle == user.handle then
+                        clients
+
+                    else
+                        case player_two.state of
+                            Connectivity.Connected client ->
+                                ( client, player_two ) :: clients
+
+                            Connectivity.Disconnected ->
+                                clients
+
+                Nothing ->
+                    if game.player_one.handle == user.handle then
+                        clients
+
+                    else
+                        case game.player_one.state of
+                            Connectivity.Connected client ->
+                                ( client, game.player_one ) :: clients
+
+                            Connectivity.Disconnected ->
+                                clients
+        )
+        []
+        list_games
+
+
+updateConnectivityOnGames : User.User -> Storage.GameStore -> Storage.GameStore
+updateConnectivityOnGames user games =
+    let
+        updated_games_of_user =
+            Game.getGames games user
+                |> List.map
+                    (\game ->
+                        if game.player_one.handle == user.handle then
+                            let
+                                updated_user =
+                                    game.player_one |> (\player -> { player | state = user.state })
+                            in
+                            { game | player_one = updated_user }
+
+                        else
+                            case game.player_two of
+                                Just player_two ->
+                                    let
+                                        updated_user =
+                                            player_two |> (\player -> { player | state = user.state })
+                                    in
+                                    { game | player_two = Just updated_user }
+
+                                Nothing ->
+                                    game
+                    )
+    in
+    List.foldl
+        (\game dict ->
+            case Dict.get (Game.getId game.id) dict of
+                Just _ ->
+                    Dict.insert (Game.getId game.id) game dict
+
+                Nothing ->
+                    dict
+        )
+        games
+        updated_games_of_user
--- src/Backend/UpdateFromFrontend.elm
+++ src/Backend/UpdateFromFrontend.elm
@@ -3,12 +3,11 @@ module Backend.UpdateFromFrontend exposing (..)
 import Dict
 import Lamdera exposing (ClientId, SessionId)
 import Types
-import Types.Storage.Registration as Registration
-import Lamdera
-import Types.Storage.Response as Response
 import Types.Storage.Auth as Auth
-import Types.Storage.Game as Game
 import Types.Storage.Connectivity as Connectivity
+import Types.Storage.Game as Game
+import Types.Storage.Registration as Registration
+import Types.Storage.Response as Response
 import Types.Storage.Storage as Storage
 import Types.Storage.User as User
 
@@ -44,7 +43,7 @@ updateFromFrontend sessionId clientId msg model =
 
                 Registration.Failure response ->
                     ( model
-                    , Lamdera.sendToFrontend clientId (Types.RegistrationResponse <| Response.FailureRegistration response) 
+                    , Lamdera.sendToFrontend clientId (Types.RegistrationResponse <| Response.FailureRegistration response)
                     )
 
         Types.LoginUser reqs ->
@@ -54,18 +53,22 @@ updateFromFrontend sessionId clientId msg model =
                         updated_connectivity =
                             updateConnectivityOnGames user model.game_store
 
-                        participating_games =
+                        clients_to_update =
                             Game.getFullGames updated_connectivity user
                                 |> clientsToUpdateGames user
-                        
+
                         cmds =
-                            List.map (\(client, player) ->
-                                Lamdera.sendToFrontend client (Types.RequestGamesResponse ( Response.SuccessRequestGames <| Game.getGames model.game_store player ))
-                            ) participating_games
-                        
-                    in 
+                            List.map
+                                (\( client, player ) ->
+                                    Lamdera.sendToFrontend client (Types.RequestGamesResponse (Response.SuccessRequestGames <| Game.getGames model.game_store player))
+                                )
+                                clients_to_update
+                    in
                     ( { model | game_store = updated_connectivity }
-                    , Lamdera.sendToFrontend clientId (Types.LoginResponse <| Response.SuccessLogin { user | state = Connectivity.Connected clientId })
+                    , Cmd.batch
+                        (Lamdera.sendToFrontend clientId (Types.LoginResponse <| Response.SuccessLogin { user | state = Connectivity.Connected clientId })
+                            :: cmds
+                        )
                     )
 
                 Auth.Fail response ->
@@ -81,7 +84,7 @@ updateFromFrontend sessionId clientId msg model =
             ( { model
                 | seed = next_seed
                 , game_store = Dict.insert (Game.getId result.id) result model.game_store
-            }
+              }
             , Lamdera.sendToFrontend clientId (Types.CreateGameRespnse <| Response.SuccessCreateGame result)
             )
 
@@ -92,13 +95,12 @@ updateFromFrontend sessionId clientId msg model =
             in
             ( model
             , Lamdera.sendToFrontend clientId (Types.RequestGamesResponse <| Response.SuccessRequestGames games)
-            
             )
 
         Types.JoinGame reqs ->
             case Game.joinGame model.game_store reqs of
                 Game.Success game ->
-                    ( { model | game_store = Dict.insert (Game.getId game.id) game model.game_store }            
+                    ( { model | game_store = Dict.insert (Game.getId game.id) game model.game_store }
                     , Lamdera.sendToFrontend clientId (Types.JoinGameResponse <| Response.SuccessJoinGame game)
                     )
 
@@ -106,7 +108,7 @@ updateFromFrontend sessionId clientId msg model =
                     ( model
                     , Lamdera.sendToFrontend clientId (Types.JoinGameResponse <| Response.FailureJoinGame error)
                     )
-        
+
         Types.UpdateGame reqs ->
             case Game.updateGame model.game_store reqs of
                 Game.Updated games ->
@@ -119,61 +121,75 @@ updateFromFrontend sessionId clientId msg model =
                     , Lamdera.sendToFrontend clientId (Types.UpdateGameResponse <| Response.FailureUpdateGame error)
                     )
 
-clientsToUpdateGames : User.User -> List Game.Game -> List (ClientId, User.User)
+
+clientsToUpdateGames : User.User -> List Game.Game -> List ( ClientId, User.User )
 clientsToUpdateGames user list_games =
-    List.foldl (\game clients ->
-        case game.player_two of
-            Just player_two ->
-                if player_two.handle == user.handle then
-                    clients
-                else
-                    case player_two.state of
-                        Connectivity.Connected client ->
-                            (client, player_two) :: clients
-                        Connectivity.Disconnected ->
-                            clients
-                
-            Nothing ->
-                if game.player_one.handle == user.handle then
-                    clients
-                else
-                    case game.player_one.state of
-                        Connectivity.Connected client ->
-                            (client, game.player_one) :: clients
-                        Connectivity.Disconnected ->
-                            clients
-    ) [] list_games
+    List.foldl
+        (\game clients ->
+            case game.player_two of
+                Just player_two ->
+                    if player_two.handle == user.handle then
+                        clients
+
+                    else
+                        case player_two.state of
+                            Connectivity.Connected client ->
+                                ( client, player_two ) :: clients
+
+                            Connectivity.Disconnected ->
+                                clients
+
+                Nothing ->
+                    if game.player_one.handle == user.handle then
+                        clients
+
+                    else
+                        case game.player_one.state of
+                            Connectivity.Connected client ->
+                                ( client, game.player_one ) :: clients
+
+                            Connectivity.Disconnected ->
+                                clients
+        )
+        []
+        list_games
+
 
 updateConnectivityOnGames : User.User -> Storage.GameStore -> Storage.GameStore
 updateConnectivityOnGames user games =
     let
         updated_games_of_user =
             Game.getGames games user
-                |> List.map (\game ->
-                    if game.player_one.handle == user.handle then
-                        let
-                            updated_user =
-                                game.player_one |> (\player -> { player | state = user.state })
-                        in
-                        { game | player_one = updated_user }
-                    else
-                        case game.player_two of
-                            Just player_two ->
-                                let
-                                    updated_user =
-                                        player_two |> (\player -> { player | state = user.state })
-                                in
-                                { game | player_two = Just updated_user }
+                |> List.map
+                    (\game ->
+                        if game.player_one.handle == user.handle then
+                            let
+                                updated_user =
+                                    game.player_one |> (\player -> { player | state = user.state })
+                            in
+                            { game | player_one = updated_user }
 
-                            Nothing ->
-                                game
-                )
+                        else
+                            case game.player_two of
+                                Just player_two ->
+                                    let
+                                        updated_user =
+                                            player_two |> (\player -> { player | state = user.state })
+                                    in
+                                    { game | player_two = Just updated_user }
+
+                                Nothing ->
+                                    game
+                    )
     in
-    List.foldl (\game dict ->
-        case Dict.get (Game.getId game.id) dict of
-            Just _ ->
-                Dict.insert (Game.getId game.id) game dict
+    List.foldl
+        (\game dict ->
+            case Dict.get (Game.getId game.id) dict of
+                Just _ ->
+                    Dict.insert (Game.getId game.id) game dict
 
-            Nothing ->
-                dict
-    ) games updated_games_of_user
+                Nothing ->
+                    dict
+        )
+        games
+        updated_games_of_user
