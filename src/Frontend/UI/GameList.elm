ccfc6ae47f7be22a89dfcae9b8eca3d58a76a2d1
Verge <m.verge97@gmail.com>
2023-09-14 18:24:18 -0700

    added some stuff to game list with game over.

--- src/Frontend/UI/GameList.elm
+++ src/Frontend/UI/GameList.elm
@@ -11,6 +11,7 @@ import Types.Board as Board
 import Types.Storage.Connectivity as Connectivity
 import Types.Storage.Game as StorageGame
 import Types.Storage.User as User
+import Types.Victory as Victory
 
 
 root : User.User -> List StorageGame.Game -> HS.Html Types.FrontendMsg
@@ -25,8 +26,94 @@ root logged_in_user list_games =
             , TW.justify_center
             ]
         ]
-        (List.map (gameCard logged_in_user) list_games)
+        (List.map ( gameCardLayer logged_in_user) list_games
+        )
 
+gameCardLayer : User.User -> StorageGame.Game -> HS.Html Types.FrontendMsg
+gameCardLayer logged_in_user game =
+    case game.path_to_victory of
+        Victory.Acheived player ->
+            let
+                victor =
+                    case game.player_two of
+                        Just player_two ->
+                            if player.handle == player_two.handle then
+                                player_two
+
+                            else
+                                game.player_one
+
+                        Nothing ->
+                            logged_in_user
+            in
+            gameCardGameOver logged_in_user victor game
+
+        Victory.Unacheived ->
+            gameCard logged_in_user game
+
+gameCardGameOver : User.User -> User.User -> StorageGame.Game -> HS.Html Types.FrontendMsg
+gameCardGameOver logged_in_user victor game =
+    let
+        base_css =
+            [ TW.box_border
+            , TW.flex
+            , TW.flex_col
+            , TW.p_3
+            , TW.items_start
+            , TW.w_5over12
+            , TW.m_2
+            , TW.bg_color TW.stone_600
+            , Css.cursor Css.pointer
+            ]
+
+        (updated_css, phrase) =
+            if logged_in_user.id == victor.id then
+                ( List.append 
+                    base_css 
+                    [ TW.bg_color TW.green_500 
+                    , TW.bg_opacity_40                               
+                    ]
+                , gameOverPhrase "YOU" (game.turn - 1) "Way to go!"
+                )
+                
+            else
+                ( List.append 
+                    base_css 
+                    [ TW.bg_color TW.red_500
+                    , TW.bg_opacity_40                         
+                    ]
+                , gameOverPhrase victor.handle (game.turn - 1) "Lame sauce."
+                ) 
+    in
+    HS.div
+        [ HSA.css updated_css 
+        , HSE.onClick <| Types.SelectGame game.id
+        ]
+        [ HS.h1
+            [ HSA.css
+                [ TW.m_2
+                ]
+            ]
+            [ HS.text game.game_name
+            ]
+        , HS.div
+            [ HSA.css
+                [ TW.box_border
+                , TW.pl_4
+                ]
+            ]
+            [ gameTypeView game.board
+            , HS.div
+                []
+                [ HS.text phrase
+                ]
+            , viewOpponent logged_in_user game.player_one game.player_two game.id
+            -- , currentPlayer game.current_player
+            ]
+        ]
+gameOverPhrase : String -> Int -> String -> String
+gameOverPhrase victor_handle num_turns phrase =
+    victor_handle ++ " won in " ++ String.fromInt num_turns ++ " turns! " ++ phrase
 
 gameCard : User.User -> StorageGame.Game -> HS.Html Types.FrontendMsg
 gameCard logged_in_user game =
@@ -61,6 +148,12 @@ gameCard logged_in_user game =
                         , TW.bg_opacity_40                         
                         ] 
                     )
+
+        current_player =
+            if logged_in_user.id == game.current_player.id then
+                "YOU"
+            else
+                game.current_player.handle
     in
     HS.div
         (HSA.css updated_css :: interactivity)
@@ -80,9 +173,10 @@ gameCard logged_in_user game =
             [ gameTypeView game.board
             , HS.div
                 []
-                [ HS.text <| "Turn " ++ String.fromInt game.turn
+                [ HS.text <| "Turn " ++ String.fromInt game.turn ++ " and waiting on " ++ current_player ++ "!"
                 ]
             , viewOpponent logged_in_user game.player_one game.player_two game.id
+            -- , currentPlayer game.current_player
             ]
         ]
 
@@ -106,6 +200,12 @@ gameTypeView board =
         [ HS.text board_type
         ]
 
+currentPlayer : User.User -> HS.Html Types.FrontendMsg
+currentPlayer user =
+    HS.div
+        []
+        [ HS.text <| "It's " ++ user.handle ++ "'s turn"
+        ]
 
 viewOpponent : User.User -> User.User -> Maybe User.User -> StorageGame.GameId -> HS.Html Types.FrontendMsg
 viewOpponent logged_in player_one m_player_two game_id =
@@ -143,13 +243,16 @@ viewOpponent logged_in player_one m_player_two game_id =
                     [ HSA.css
                         [ TW.box_border
                         , TW.flex
-                        , TW.space_x_2
+                        -- , TW.space_x_2
                         , TW.items_center
-                        , TW.justify_center
+                        -- , TW.justify_center
                         ]
                     ]
                     [ HS.div
-                        []
+                        [ HSA.css
+                            [ TW.mr_2                                
+                            ]                            
+                        ]
                         [ HS.text user.handle
                         ]
                     , connection
